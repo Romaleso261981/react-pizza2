5741a955d390e5413b94c5c09cbad891bad8d42d
Roman lesyo <ladiginscormag@gmail.com>
2023-05-22 13:37:55 +0300

    added pages HomePage, Notfound

---
+++ src/pages/HomePages/HomePage.jsx
@@ -0,0 +1,30 @@
+import Header from "./components/Header/Header";
+import Catigories from "./components/Catigories/Catigories";
+import Sort from "./components/Sort/Sort";
+import CardList from "./components/CardList/CardList";
+import Loader from "./components/Loader/Loader";
+import "./HomePage.scss";
+
+function App({ setCategory, setSorting, loading, data }) {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Catigories setCategory={setCategory} />
+            <Sort
+              setSorting={setSorting}
+              title={"Сортування за"}
+              sort={["популярностю", "ціною", "алфавітом"]}
+            />
+          </div>
+          {!loading && <Loader />}
+          {loading && <CardList data={data} />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
